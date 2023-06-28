1733ad5b63dbf4591a4c38d972db42e883ea30d2
DiegoJ4V <diegoj4v@gmail.com>
2023-06-28 10:30:08 -0600

    Add divider card

---
+++ fronted/src/components/DividerCard/DividerCard.stories.js
@@ -0,0 +1,21 @@
+import { DividerCard } from "./DividerCard";
+
+export default {
+   title: "DividerCard",
+   component: DividerCard,
+   tags: ['autodocs']
+};
+
+const Template = ({ label, ...args }) => { 
+   return DividerCard({ label, ...args });
+};
+
+export const Default  = Template.bind({});
+Default.args = {
+   automated: true,
+   transferAccount: "0",
+   name: "Cuenta",
+   amount: "$432003.00",
+   type: "Wire Transfer",
+   time: "2024/02/03  11:34:23"
+};
