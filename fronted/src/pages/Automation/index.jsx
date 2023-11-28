25b3e918f4c90d380feb2d5ff1cc11dd5584a445
DiegoJ4V <diegoj4v@gmail.com>
2023-11-28 13:52:18 -0600

    Change time period menu format, and env.example

--- fronted/src/pages/Automation/index.jsx
+++ fronted/src/pages/Automation/index.jsx
@@ -119,18 +119,19 @@ export const Automation = () => {
                   supportiveText={error.hoursToNextExecution}
                   modalParameters={[{
                      label: t.modalParameters[0],
+                     textFieldType: TextFieldTypes.DEFAULT,
                      initialInputType: InputTypes.NUMBER,
-                     textFieldType: TextFieldTypes.DEFAULT
                   }, {
                      label: t.modalParameters[1],
+                     textFieldType: TextFieldTypes.MENU,
                      initialInputType: InputTypes.NUMBER,
-                     textFieldType: TextFieldTypes.DEFAULT,
-                     max: 6
+                     menuParameters: [1, 2, 3, 4, 5, 6]
                   }, {
                      label: t.modalParameters[2],
+                     textFieldType: TextFieldTypes.MENU,
                      initialInputType: InputTypes.NUMBER,
-                     textFieldType: TextFieldTypes.DEFAULT,
-                     max: 23
+                     menuParameters: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 
+                        12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
                   }]}
                   isDisable={isLoading}
                />
