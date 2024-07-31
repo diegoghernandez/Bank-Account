77b8de146fefd5d64f61fe9df9e821b070cf795c
diegoghernandez <diegoj4v@gmail.com>
2024-07-31 12:19:59 -0600

    Update open api links

--- backend/src/main/java/com/bankaccount/back/web/config/OpenApiConfig.java
+++ backend/src/main/java/com/bankaccount/back/web/config/OpenApiConfig.java
@@ -9,14 +9,14 @@ import io.swagger.v3.oas.annotations.info.License;
         info = @Info(
                 contact = @Contact(
                         name = "Diego",
-                        email = "diegoj4v@gmail.com",
-                        url = "https://diegoj4v.github.io/My-website"
+                        email = "guadarramahernandezdiego@gmail.com",
+                        url = "https://diego-g-hernandez.pages.dev/"
                 ),
                 title = "OpenApi specification - Bank",
                 description = "OpenApi documentation for the bank system",
                 license = @License(
                         name = "MIT License",
-                        url = "https://github.com/DiegoJ4V/Bank-Account/blob/main/LICENSE"
+                        url = "https://github.com/diegoghernandez/Bank-Account/blob/main/LICENSE"
                 )
         )
 )
